94d64f407be8c3172d7ec7911f0b0a3775217bad
Arkadiusz <arekm634@gmail.com>
2025-06-05 22:58:55 +0200

    ..

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,38 +1,124 @@
-﻿namespace Kaenen;
-
-partial class Form1
+﻿namespace Kaenen
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null))
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnWczytaj;
+        private System.Windows.Forms.Button btnOblicz;
+        private System.Windows.Forms.ComboBox cmbMetryki;
+        private System.Windows.Forms.TextBox txtWyniki;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblMetryka;
+        private System.Windows.Forms.Label lblWyniki;
+        private System.Windows.Forms.NumericUpDown nudK;
+        private System.Windows.Forms.Label lblK;
+
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
+
+        private void InitializeComponent()
+        {
+            this.btnWczytaj = new System.Windows.Forms.Button();
+            this.btnOblicz = new System.Windows.Forms.Button();
+            this.cmbMetryki = new System.Windows.Forms.ComboBox();
+            this.txtWyniki = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblMetryka = new System.Windows.Forms.Label();
+            this.lblWyniki = new System.Windows.Forms.Label();
+            this.nudK = new System.Windows.Forms.NumericUpDown();
+            this.lblK = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudK)).BeginInit();
+            this.SuspendLayout();
+
+            this.btnWczytaj.Location = new System.Drawing.Point(12, 12);
+            this.btnWczytaj.Name = "btnWczytaj";
+            this.btnWczytaj.Size = new System.Drawing.Size(100, 30);
+            this.btnWczytaj.TabIndex = 0;
+            this.btnWczytaj.Text = "Wczytaj dane";
+            this.btnWczytaj.UseVisualStyleBackColor = true;
+
+            this.lblMetryka.AutoSize = true;
+            this.lblMetryka.Location = new System.Drawing.Point(12, 55);
+            this.lblMetryka.Name = "lblMetryka";
+            this.lblMetryka.Size = new System.Drawing.Size(50, 13);
+            this.lblMetryka.TabIndex = 1;
+            this.lblMetryka.Text = "Metryka:";
+
+            this.cmbMetryki.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMetryki.Location = new System.Drawing.Point(70, 52);
+            this.cmbMetryki.Name = "cmbMetryki";
+            this.cmbMetryki.Size = new System.Drawing.Size(150, 21);
+            this.cmbMetryki.TabIndex = 2;
+
+            this.btnOblicz.Enabled = false;
+            this.btnOblicz.Location = new System.Drawing.Point(380, 50);
+            this.btnOblicz.Name = "btnOblicz";
+            this.btnOblicz.Size = new System.Drawing.Size(100, 25);
+            this.btnOblicz.TabIndex = 3;
+            this.btnOblicz.Text = "Oblicz";
+            this.btnOblicz.UseVisualStyleBackColor = true;
+
+            this.lblK.AutoSize = true;
+            this.lblK.Location = new System.Drawing.Point(240, 55);
+            this.lblK.Name = "lblK";
+            this.lblK.Size = new System.Drawing.Size(17, 13);
+            this.lblK.TabIndex = 7;
+            this.lblK.Text = "k:";
+
+            this.nudK.Location = new System.Drawing.Point(260, 52);
+            this.nudK.Maximum = new decimal(new int[] { 20, 0, 0, 0 });
+            this.nudK.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            this.nudK.Name = "nudK";
+            this.nudK.Size = new System.Drawing.Size(50, 20);
+            this.nudK.TabIndex = 8;
+            this.nudK.Value = new decimal(new int[] { 3, 0, 0, 0 });
+
+            this.lblWyniki.AutoSize = true;
+            this.lblWyniki.Location = new System.Drawing.Point(12, 85);
+            this.lblWyniki.Name = "lblWyniki";
+            this.lblWyniki.Size = new System.Drawing.Size(40, 13);
+            this.lblWyniki.TabIndex = 4;
+            this.lblWyniki.Text = "Wyniki:";
+
+            this.txtWyniki.Location = new System.Drawing.Point(12, 105);
+            this.txtWyniki.Multiline = true;
+            this.txtWyniki.Name = "txtWyniki";
+            this.txtWyniki.ReadOnly = true;
+            this.txtWyniki.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtWyniki.Size = new System.Drawing.Size(560, 300);
+            this.txtWyniki.TabIndex = 5;
+            this.txtWyniki.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 420);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(100, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Nie wczytano danych";
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 450);
+            this.Controls.Add(this.lblK);
+            this.Controls.Add(this.nudK);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtWyniki);
+            this.Controls.Add(this.lblWyniki);
+            this.Controls.Add(this.btnOblicz);
+            this.Controls.Add(this.cmbMetryki);
+            this.Controls.Add(this.lblMetryka);
+            this.Controls.Add(this.btnWczytaj);
+            this.Name = "Form1";
+            this.Text = "Kaenen - Kalkulator metryk";
+            ((System.ComponentModel.ISupportInitialize)(this.nudK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
-
-    #endregion
 }
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,185 @@
-namespace Kaenen;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace Kaenen
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        private delegate double MetrykaDelegate(double[] a, double[] b);
+        private double[][] dane;
+        private string[] etyk;
+        private int k = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            btnWczytaj.Click += BtnWczytaj_Click;
+            btnOblicz.Click += BtnOblicz_Click;
+            nudK.ValueChanged += NudK_ValueChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var metody = typeof(Form1).GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+                .Where(m => m.ReturnType == typeof(double) &&
+                           m.GetParameters().Length == 2 &&
+                           m.GetParameters()[0].ParameterType == typeof(double[]) &&
+                           m.GetParameters()[1].ParameterType == typeof(double[]))
+                .ToArray();
+
+            cmbMetryki.DataSource = metody;
+            cmbMetryki.DisplayMember = "Name";
+        }
+
+        private void BtnWczytaj_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Filter = "Pliki tekstowe (*.txt)|*.txt";
+                dlg.Title = "Wybierz plik z danymi";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    WczytajDane(dlg.FileName);
+                }
+            }
+        }
+
+        private void WczytajDane(string sciezka)
+        {
+            try
+            {
+                string[] linie = File.ReadAllLines(sciezka);
+                dane = new double[linie.Length][];
+                etyk = new string[linie.Length];
+
+                for (int i = 0; i < linie.Length; i++)
+                {
+                    string[] czesci = linie[i].Split(' ');
+                    etyk[i] = czesci[czesci.Length - 1];
+                    dane[i] = new double[czesci.Length - 1];
+
+                    for (int j = 0; j < czesci.Length - 1; j++)
+                    {
+                        if (double.TryParse(czesci[j].Replace('.', ','), out double wartosc))
+                            dane[i][j] = wartosc;
+                    }
+                }
+
+                lblStatus.Text = $"Wczytano {dane.Length} rekordów, {dane[0].Length} cech";
+                btnOblicz.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd wczytywania: {ex.Message}", "Błąd");
+            }
+        }
+
+        private void NudK_ValueChanged(object sender, EventArgs e)
+        {
+            k = (int)nudK.Value;
+        }
+
+        private void BtnOblicz_Click(object sender, EventArgs e)
+        {
+            if (dane == null || dane.Length < 2)
+            {
+                MessageBox.Show("Najpierw wczytaj dane!", "Błąd");
+                return;
+            }
+
+            MethodInfo wybMetoda = cmbMetryki.SelectedItem as MethodInfo;
+            if (wybMetoda == null) return;
+
+            string wynik = $"═══ WYNIKI OBLICZEŃ - {wybMetoda.Name.Replace("Metryka", "")} (k={k}) ═══\n\n";
+
+            var klasy = new[] { "1", "2", "3" };
+            foreach (string klasa in klasy)
+            {
+                wynik += $"▬▬▬ KLASA {klasa} ▬▬▬\n";
+                var objKlasy = dane.Select((d, i) => new { dane = d, idx = i, etyk = etyk[i] })
+                                  .Where(x => x.etyk == klasa)
+                                  .Take(k)
+                                  .ToArray();
+
+                for (int i = 0; i < objKlasy.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < objKlasy.Length; j++)
+                    {
+                        double odl = (double)wybMetoda.Invoke(this, new object[] { objKlasy[i].dane, objKlasy[j].dane });
+                        wynik += $"  Obj[{objKlasy[i].idx + 1:D3}] ↔ Obj[{objKlasy[j].idx + 1:D3}]: {odl:F4}\n";
+                    }
+                }
+                wynik += "\n";
+            }
+
+            wynik += $"▬▬▬ MIĘDZYKLASOWE (pierwsze {k * 2}) ▬▬▬\n";
+            var obj1 = dane.Select((d, i) => new { dane = d, idx = i, etyk = etyk[i] })
+                          .Where(x => x.etyk == "1").Take(k).ToArray();
+            var obj2 = dane.Select((d, i) => new { dane = d, idx = i, etyk = etyk[i] })
+                          .Where(x => x.etyk == "2").Take(k).ToArray();
+            var obj3 = dane.Select((d, i) => new { dane = d, idx = i, etyk = etyk[i] })
+                          .Where(x => x.etyk == "3").Take(k).ToArray();
+
+            int licz = 0;
+            foreach (var o1 in obj1)
+            {
+                foreach (var o2 in obj2)
+                {
+                    if (licz++ >= k * 2) break;
+                    double odl = (double)wybMetoda.Invoke(this, new object[] { o1.dane, o2.dane });
+                    wynik += $"  Kl1[{o1.idx + 1:D3}] ↔ Kl2[{o2.idx + 1:D3}]: {odl:F4}\n";
+                }
+                if (licz >= k * 2) break;
+            }
+
+            txtWyniki.Text = wynik;
+        }
+
+        private double MetrykaEuklidesowa(double[] a, double[] b)
+        {
+            double suma = 0.0;
+            for (int i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                suma += Math.Pow(a[i] - b[i], 2.0);
+            }
+            return Math.Sqrt(suma);
+        }
+
+        private double MetrykaManhatanska(double[] a, double[] b)
+        {
+            double suma = 0.0;
+            for (int i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                suma += Math.Abs(a[i] - b[i]);
+            }
+            return suma;
+        }
+
+        private double MetrykaMinkowskiego(double[] a, double[] b)
+        {
+            double suma = 0.0;
+            double p = k;
+            for (int i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                suma += Math.Pow(Math.Abs(a[i] - b[i]), p);
+            }
+            return Math.Pow(suma, 1.0 / p);
+        }
+
+        private double MetrykaCzebyszewa(double[] a, double[] b)
+        {
+            double maks = 0.0;
+            for (int i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                double rozn = Math.Abs(a[i] - b[i]);
+                if (rozn > maks) maks = rozn;
+            }
+            return maks;
+        }
     }
 }
